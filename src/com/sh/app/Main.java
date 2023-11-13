d9f2f2ba3b0c86b2b9cc9a40e11b3aa6615e1eec
dlachdla <chyolim4692@naver.com>
2023-11-13 11:36:54 +0900

    Pig#jump 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -2,17 +2,19 @@ package com.sh.app;
 
 import com.sh.app.animal.Cat;
 import com.sh.app.animal.Dog;
+import com.sh.app.animal.Pig;
 import com.sh.app.animal.Tiger;
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO WORLD");
-		System.out.println("HELLO GIT");
+		System.out.println("Hello World");
+		System.out.println("Hello Git");
 		
 		new Dog().bark();
 		new Cat().jump();
 		new Tiger().jump();
+		new Pig().jump();
 	}
 
 }
---
+++ src/com/sh/app/animal/Pig.java
@@ -0,0 +1,7 @@
+package com.sh.app.animal;
+
+public class Pig {
+	public void jump() {
+		System.out.println("돼지가 점프!!");
+	}
+}
